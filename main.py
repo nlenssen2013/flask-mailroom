e248d70b49ec5c4cd14722fbd74b7969d9d64cf3
Nick Lenssen <nicklenssen4@gmail.com>
2020-06-21 16:42:52 -0500

    6738

--- main.py
+++ main.py
@@ -105,5 +105,5 @@ def query():
     return render_template("query.jinja2")
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 6738))
     app.run(host='0.0.0.0', port=port)
